a5e12acf6b72f3d583924238b966e4a4c91f495c
venkey <venkeymee@gmail.com>
2021-12-20 13:16:29 +0400

    config files updated
    config files updated

--- config-client/src/test/java/com/vnky/configclient/ConfigClientApplicationTests.java
+++ config-client/src/test/java/com/vnky/configclient/ConfigClientApplicationTests.java
@@ -1,12 +1,10 @@
 package com.vnky.configclient;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConfigClientApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
--- config-server/src/main/java/com/vky/Application.java
+++ config-server/src/main/java/com/vky/Application.java
@@ -1,11 +1,13 @@
 package com.vky;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
